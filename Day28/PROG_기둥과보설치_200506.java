7c0b5466ba213bc54c13f98cabaf648e6640fe07
김기훈 <kh06089@naver.com>
2020-05-06 21:38:28 +0900

    Day_28 업로드

---
+++ Day28/PROG_기둥과보설치_200506.java
@@ -0,0 +1,66 @@
+﻿class Solution {
+    static int N;
+    static boolean[][][] map;
+    static int cnt = 0;
+    public int[][] solution(int n, int[][] build_frame) {
+        N = n;
+        map = new boolean[N+3][N+3][2]; //0:기둥, 1:보. + 양옆 비교때문에 배열 확장.
+        for(int i=0; i<build_frame.length; i++){
+            //System.out.println(cnt);
+            int dx = build_frame[i][0] + 1;
+            int dy = build_frame[i][1] + 1;
+            int type = build_frame[i][2];
+            int mode = build_frame[i][3];
+            if(mode == 0){
+                remove(dx,dy,type);
+            }
+            else{
+                if(can_make(dx,dy,type)){
+                    map[dx][dy][type] = true;
+                    cnt++;
+                }
+            }
+        }//end for.
+        int[][] answer = new int[cnt][3];
+        int top = 0;
+        for (int i = 1; i <= N + 1; i++) {
+            for (int j = 1; j <= N + 1; j++) {
+                if (map[i][j][0]) answer[top++] = new int[] {i-1, j-1, 0};
+                if (map[i][j][1]) answer[top++] = new int[] {i-1, j-1, 1};
+            }
+        }
+        return answer;
+    }//end main.
+    public static boolean can_make(int x, int y, int type){
+        if(type == 0){//기둥일때.
+            if(y == 1 || map[x][y-1][0] || map[x][y][1] || map[x-1][y][1]){//바닥 + 기둥위 + 보위 2가지 가능.
+                return true;
+            }
+            return false;
+        }
+        else{//보일때.
+            if(map[x][y-1][0] || map[x+1][y-1][0] || (map[x-1][y][1] && map[x+1][y][1])){ //기둥위 2가지 + 보사이에 가능.
+               return true;
+            }
+            return false;
+        }
+    }//end make.
+    public static void remove(int x, int y, int type){
+        map[x][y][type] = false;
+        boolean can = true;
+ex:     for (int i = 1; i <= N + 1; i++) {
+            for (int j = 1; j <= N + 1; j++) {
+                if (map[i][j][0] && !can_make(i, j, 0)) { //현재 설치된 기둥 + 조건성립 X.
+                    can = false;
+                    break ex;
+                }
+                if (map[i][j][1] && !can_make(i, j, 1)) { //현재 설치된 보 + 조건성립 X.
+                    can = false;
+                    break ex;
+                }
+            }
+        }
+        if(!can) map[x][y][type] = true;
+        else cnt--;
+    }//end remove.
+}//end class.
---
+++ Day28/PROG_외벽점검_200505.java
@@ -0,0 +1,59 @@
+﻿import java.util.*;
+
+class Solution {
+    static int N;
+    static ArrayList<Integer> list = new ArrayList<Integer>();
+    static int[] div;
+    static boolean[] selected;
+    static int[] weak_list;
+    static boolean end;
+    static int answer = -1;
+    public int solution(int n, int[] weak, int[] dist) {
+        N = n;
+        weak_list = weak;
+        for(int i=1; i<=dist.length; i++){
+            list.add(dist[dist.length-i]);
+            div = new int[i];
+            selected = new boolean[i];
+            divide(i, 1, weak_list.length) ;
+            if(end) return answer;
+        }
+        return answer;
+    }//end main.
+    public static void divide(int num, int now, int total){
+        if(num==now){
+            div[now-1] = total;
+            for(int i=0; i<weak_list.length; i++){
+                check(i,0);
+                if(end) return;
+            }
+            return ;
+        }
+        for(int i=1; i<=total-(num-now); i++){
+            div[now-1] = i;
+            divide(num,now+1,total-i);
+        }
+    }//end divide.
+    public static void check(int start, int cnt){
+        if(cnt == div.length){
+            answer = cnt;
+            end = true;
+            return ;
+        }
+        int length = 0;
+        for(int i=1; i<div[cnt]; i++){
+            int size = weak_list.length;
+            int tmp = weak_list[(start+i)%size] - weak_list[(start+i-1)%size];
+            if(tmp<0) tmp+=N;
+            length += tmp;
+        }
+        for(int i=0; i<list.size(); i++){
+            if(!selected[i] && list.get(i) >= length){
+                selected[i] = true;
+                check(start+div[cnt],cnt+1);
+                if(end) return ;
+                selected[i] = false;
+            }
+        }
+    }
+}//end solution.
